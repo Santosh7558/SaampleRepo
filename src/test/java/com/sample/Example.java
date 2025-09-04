572d2c754a2c3ec38e9b272a6a7aba057079d97c
arjun <am.puneeth@bounteous.com>
2025-09-04 14:12:13 +0530

    first commit

---
+++ src/test/java/com/sample/Example.java
@@ -0,0 +1,30 @@
+package com.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Example {
+@Test
+public void launchUrl() {
+	WebDriverManager.chromedriver().setup();
+//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//driver//chromedriver.exe");
+//	System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+	ChromeOptions options=new ChromeOptions();
+//	options.addArguments("--headless");
+//	options.addArguments("--no-sandbox");
+//	options.addArguments("--disable-dev-shm-usage");
+//	options.addArguments("--disable-gpu");
+//	options.addArguments("--remote-allow-origins=*");
+	WebDriver driver=new ChromeDriver(options);
+	driver.get("https://google.com/");
+	driver.quit();
+
+}
+
+
+}
